89d5ce134f218b8b8be5a0d67652e4e93b196949
Jonathan Pulfer <JonPulfer@server.fake>
2013-09-30 19:39:18 +0100

    Changed to ensure each char is unique in the password.

--- pgen.go
+++ pgen.go
@@ -2,32 +2,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"time"
-	"flag"
 )
 
 var pwdlen int
+
 func init() {
-	flag.IntVar(&pwdlen, "pwdlen", 12, "Desired length of password to generate")
+	flag.IntVar(&pwdlen, "pwdlen", 12, "Desired length of password "+
+		"to generate")
 	flag.Parse()
 }
 
 // Generate a random string.
-func randomString (l int) string {
+func randomString(l int) string {
 	bytes := make([]byte, l)
 	for i := 0; i < l; i++ {
-		bytes[i] = byte(randInt(33,126))
+	REDO:
+		c := byte(randInt(33, 126))
+		if !checkRepeat(bytes, c) {
+			bytes[i] = c
+		} else {
+			goto REDO
+		}
 	}
 	return string(bytes)
 }
 
-func randInt(min int , max int) int {
-	    return min + rand.Intn(max-min)
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
 
-func main () {
-	rand.Seed( time.Now().UTC().UnixNano())
+func checkRepeat(b []byte, c byte) bool {
+	for _, n := range b {
+		if n == c {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Println(randomString(pwdlen))
 }
